787d4e1787bcf9a4e1d334f0a020f1aecbb36ef5
Batr Kanzitelli <kanzitdinov@gmail.com>
2019-09-18 18:34:32 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/team/ggc/batr/good_news_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package team.ggc.batr.good_news_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
